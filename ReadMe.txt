96176dfce9a55af2bf99690a9e0f1ac8cc49d543
Verain <verainthakur1183@gmail.com>
2022-01-11 17:23:45 +0530

    Final product

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Dated: 11/ 1/ 2022
+We are going to create an application to perform the basic CRUD operations. 
+
+Step 1: create the angular application with ng new <filename> command.
+Step 2: Open the file in code and run the command npm install -g json-server to install a fake JSON server to store th data.
+Step 3: Import the necessary bootstarp files and paste them in 'index.html'.
+Step 4: To run the json server command is: 'json-server --watch db.json'.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
